e87b71d8e9aa1f766fcc1be45f7a31837b14fba9
animesh987 <58603150+animesh987@users.noreply.github.com>
2019-12-07 00:16:07 +0530

    Update file1.c

--- file1.c
+++ file1.c
@@ -1,5 +1,7 @@
+void fill(int*,int);           
+void sumarr(const int*,int);   
+void display(const int*,int);  
 
-int i;
 int main() 
 {
   int n=10;
@@ -10,12 +12,12 @@ int main()
   return 0;
 }
 void fill(int* parr,int n) {     
-  for(i=0;i<n;i++)
+  for(int i=0;i<n;i++)
     parr[i]=i*10+1;  
 }
 void sumarr(const int* parr, int n) {
   int sum=0;
-  for(i=0;i<n;i++)
+  for(int i=0;i<n;i++)
     sum+=*parr++;   
 }
 void display(const int *parr,int n) {
