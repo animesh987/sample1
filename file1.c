9f2e9422e590489d9ddf1cfb7788df041f1c4676
animesh987 <58603150+animesh987@users.noreply.github.com>
2019-12-07 00:07:34 +0530

    Create file1.c

---
+++ file1.c
@@ -0,0 +1,26 @@
+void fill(int*,int);           
+void sumarr(const int*,int);    
+void display(const int*,int); 
+int i;
+int main() 
+{
+  int n=10;
+  int arr[10];
+  fill(arr,n);
+  sumarr(arr,n);
+  display(arr,n);  
+  return 0;
+}
+void fill(int* parr,int n) {     
+  for(i=0;i<n;i++)
+    parr[i]=i*10+1;  
+}
+void sumarr(const int* parr, int n) {
+  int sum=0;
+  for(i=0;i<n;i++)
+    sum+=*parr++;   
+}
+void display(const int *parr,int n) {
+  for(i=0;i<n;i++)
+    printf("%d\n",parr[i]);  
+}
